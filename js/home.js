69e2f3eac0ab6941e72714dd2343702a61b4cea4
kalyangoud145 <kalyangoud145@gmail.com>
2021-01-01 17:47:43 +0530

    [Kalyan] added: method to display employee data from JSON object

--- js/home.js
+++ js/home.js
@@ -1,4 +1,3 @@
-  
 window.addEventListener('DOMContentLoaded',(event)=>
 {
     createInnerHtml();
@@ -6,26 +5,65 @@ window.addEventListener('DOMContentLoaded',(event)=>
 
 var createInnerHtml=()=>
 {
-    const headerHtml= "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
     //using template literal
-    var innerHTML= `${headerHtml}
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
     <tr>
-        <td>
-            <img class="profile" alt="" src="../assets/profile-images/Ellipse -8.png">
-        </td>
-        <td>Ravula kalyan goud</td>
-        <td>Male</td>
-        <td>
-            <div class='dept-label'>HR</div>
-            <div class="dept-label">Engineering</div>
-        </td>
-        <td>444444</td>
-        <td>18 September 2020</td>
-        <td>
-            <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-            <img id="1" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
-        </td>
-    </tr>
-    `;
-    document.querySelector('#table-display').innerHTML=innerHTML;
+          <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+          <td>${empPayrollData._name}</td>
+          <td>${empPayrollData._gender}</td>
+          <td>${getDeptHtml(empPayrollData._department)}</td>
+          <td>${empPayrollData._salary}</td>
+          <td>${empPayrollData._startDate}</td>
+          <td>
+            <img id="${empPayrollData._id}" onclick= "remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+            <img id="${empPayrollData.id}" onclick= "update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+          </td>
+    </tr>`;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
 }
+// Creating Json object
+var createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+      {       
+        _name: 'Kalyan goud',
+        _gender: 'male',
+        _department: [
+            'Engineering',
+            'Finance'
+        ],
+        _salary: '500000',
+        _startDate: '29 Oct 2019',
+        _note: '',
+        _id: new Date().getTime(),
+        _profilePic: '../assets/profile-images/Ellipse -2.png'
+      },
+      {
+        _name: 'Xavier',
+        _gender: 'Female',
+        _department: [
+            'Sales'
+        ],
+        _salary: '400000',
+        _startDate: '29 Oct 2019',
+        _note: '',
+        _id: new Date().getTime() + 1,
+        _profilePic: '../assets/profile-images/Ellipse -1.png'
+      }
+    ];
+    return empPayrollListLocal;
+  }
+
+  var getDeptHtml= (deptList)=>
+  {
+      let deptHtml='';
+      for(const dept of deptList)
+      {
+          deptHtml= `${deptHtml}<div class="dept-label">${dept}</div>`
+      }
+      return deptHtml;
+  }
